d274b05018fb2bfc936599d5181b60d3615e4488
vaibhvai dhenge <vaibhavidhenge2302@gmail.com>
2026-04-16 17:29:14 +0530

    route added

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://user-management-system-8cvg.onrender.com/api',
+    baseURL: 'https://user-management-system-8cvg.onrender.com/',
 });
 
 api.interceptors.request.use((config) => {
